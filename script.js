0ceeb4bb00be463c5c7101fc80b943568306aeb4
nazhan github <nazhansukri11@gmail.com>
2022-11-17 01:00:48 +0800

    layout of quiz app

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData=[
+    {
+        question:'Which team will win World Cup 2022',
+        a:'Brazil',
+        b:'Argentina',
+        c:'France',
+        d:'England',
+        correct: 'd'
+    },
+    {
+        question:'Who will win top scorer in World Cup 2022',
+        a:'Messi',
+        b:'Ronaldo',
+        c:'Mbappe',
+        d:'Benzema',
+        correct: 'd'
+    },
+    {
+        question:'Who will win top assist in World Cup 2022',
+        a:'De Bruyne',
+        b:'Di Maria',
+        c:'Pedri',
+        d:'Guandozi',
+        correct: 'c'
+    },
+    {
+        question:'Who will win top keeper in World Cup 2022',
+        a:'Lorris',
+        b:'Martinez',
+        c:'Courtois',
+        d:'Allison',
+        correct: 'a'
+    },
+    {
+        question:'Who will win top player in World Cup 2022',
+        a:'De Bruyne',
+        b:'Messi',
+        c:'Ronaldo',
+        d:'Muller',
+        correct: 'd'
+    }
+]
