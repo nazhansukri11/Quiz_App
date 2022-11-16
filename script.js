bccb563a90f4be4344a0982adc36c2829e670e8c
nazhan github <nazhansukri11@gmail.com>
2022-11-17 02:46:26 +0800

    Add eventListener and manipulating DOM

--- script.js
+++ script.js
@@ -39,4 +39,37 @@ const quizData=[
         d:'Muller',
         correct: 'd'
     }
-]
+];
+
+
+const questionEl=document.getElementById('question');
+const a_text=document.getElementById('a_text');
+const b_text=document.getElementById('b_text');
+const c_text=document.getElementById('c_text');
+const d_text=document.getElementById('d_text');
+const btnSubmit=document.getElementById('btnSubmit');
+
+let currentQuiz=0;
+function loadQuiz(){
+        const currentQuizData=quizData[currentQuiz];
+
+    questionEl.innerText=currentQuizData.question;
+
+    a_text.innerText=currentQuizData.a;
+    b_text.innerText=currentQuizData.b;
+    c_text.innerText=currentQuizData.c;
+    d_text.innerText=currentQuizData.d;
+
+}
+loadQuiz();
+
+
+btnSubmit.addEventListener("click",()=>{
+    currentQuiz++;
+    if(currentQuiz<quizData.length){
+        loadQuiz();
+    }
+    else{
+        alert("You answer all the question")
+    }
+});
